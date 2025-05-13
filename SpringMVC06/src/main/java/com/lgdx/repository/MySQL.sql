c92c2b69bdd7e503d1df945b714f81aeeb99bfb7
suhyeon <suhyeon@sue>
2025-05-13 22:58:33 +0900

    git 시작

---
+++ SpringMVC06/src/main/java/com/lgdx/repository/MySQL.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO BOARD(TITLE, CONTENTS, WRITER)
+VALUES('나 의천인데 나만 다들 바라봤으면 좋겠다', '내가 주인공이니까♥', '조의천');
+
+INSERT INTO BOARD(TITLE, CONTENTS, WRITER)
+VALUES('지영쌤 오면 깜짝 파티해주자~', '★상자안에 내가 선물★', '김동현');
+
+INSERT INTO BOARD(TITLE, CONTENTS, WRITER)
+VALUES('동현씨 그거 하면 고소당할 듯...', '하지마셈..', '서지민');
+
+COMMIT;
+
+SELECT * FROM BOARD;
+
+
+
